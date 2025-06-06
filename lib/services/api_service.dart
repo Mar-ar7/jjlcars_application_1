5399aa7cbcb92dc5ecb5ab5fc65dd2f82e45c9cb
Mar-ar7 <daniilopeza07@gmail.com>
2025-06-06 13:08:22 -0600

    Correccion errores CRUD

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -333,7 +333,6 @@ class ApiService {
       headers: {'Content-Type': 'application/json'},
       body: json.encode({
         'tipoCita': cita.tipoCita,
-        'tipoCompra': cita.tipoCompra,
         'precio': cita.precio,
         'nombre': cita.nombre,
         'correo': cita.correo,
@@ -356,7 +355,6 @@ class ApiService {
       body: json.encode({
         'id': cita.id,
         'tipoCita': cita.tipoCita,
-        'tipoCompra': cita.tipoCompra,
         'precio': cita.precio,
         'nombre': cita.nombre,
         'correo': cita.correo,
